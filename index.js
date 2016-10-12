5e11b4686a514b2a6eaa66a9969e7d20ea09f444
Federico Brigante <bfred-it@users.noreply.github.com>
2016-10-11 22:47:12 -0700

    Extract functions for readability

--- index.js
+++ index.js
@@ -2,10 +2,14 @@
 
 const pattern = /(^|[\s\n]|<[A-Za-z]*\/?>)((?:https?|ftp):\/\/[\-A-Z0-9+\u0026\u2019@#\/%?=()~_|!:,.]*[\-A-Z0-9+\u0026@#\/%=~()_|])/gi
 
-module.exports = (content, opts = {}) => {
-  const attrs = Object.keys(opts).map(key => key === 'callback' ? '' : ` ${key}="${opts[key]}"`).join('')
+function getAttrs(key) {
+	return key === 'callback' ? '' : ` ${key}="${this[key]}"`
+}
+function replacer(match, space, url) {
+	return this.callback ? this.callback(url) : `${space}<a href="${url}"${this.attrs}>${url}</a>`
+}
 
-  return content.replace(pattern, (match, space, url) =>
-    opts.callback ? opts.callback(url) : `${space}<a href="${url}"${attrs}>${url}</a>`
-  )
+module.exports = (content, opts = {}) => {
+  opts.attrs = Object.keys(opts).map(getAttrs, opts).join('')
+  return content.replace(pattern, replacer.bind(opts))
 }
